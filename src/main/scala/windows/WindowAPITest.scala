816aaaaed877b51239cf099d90855357f1ef1b1f
“moxtong2” <512290791@qq.com>
2021-02-26 17:43:13 +0800

    2/26 v1  window 窗口操作

--- src/main/scala/windows/WindowAPITest.scala
+++ src/main/scala/windows/WindowAPITest.scala
@@ -2,6 +2,7 @@ package windows
 
 import Transform.SensorReading01
 import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -16,6 +17,8 @@ object WindowAPITest {
     //如果想保证数据处理的顺序 那么就需要设置程序运行并行度为1，
     // 如果不设置则按照cpu核数来进行计算
     environment.setParallelism(1)
+    //在代码中设置事件时间 语意
+    environment.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     //读取本地文件
     /*  val inputData = "E:\\BIGDATAWORK\\Flink_scala\\data\\file.txt"
       val inputDataStream = environment.readTextFile(inputData)*/
